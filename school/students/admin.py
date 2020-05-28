aa445f2e3d02532416d4b60044839952f0770e78
lee <derrosilee@gmail.com>
2020-05-28 07:30:20 +0300

    Update admin.py

--- school/students/admin.py
+++ school/students/admin.py
@@ -1,13 +1,6 @@
 from django.contrib import admin
 from .models import Student
-from profile_maker.models import User_Profile
-from post.models import Post, Like
-
 # Register your models here.
 
 admin.site.register(Student)
 admin.site.site_header = 'School Management System'
-admin.site.register(User_Profile)
-
-admin.site.register(Post)
-admin.site.register(Like)
